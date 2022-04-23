d8c8ab248c967baae23cc49df0bbaf4cb934b517
Marius <maas0903@gmail.com>
2022-04-23 22:17:13 +0200

    calibrated

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,55 @@
 #include <HX711.h> //You must have this library in your arduino library folder
 
-#define DOUT 23
 #define CLK 22
+#define DOUT 23
 
 HX711 scale;
 
-//Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
-float calibration_factor = -109525; //-106600 worked for my 40Kg max scale setup
+float Calibration_Factor_Of_Load_cell = -8.1;
+float U;
+float O;
 
-//=============================================================================================
-//                         SETUP
-//=============================================================================================
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("HX711 Calibration");
+    Serial.println("HX711 calibration sketch");
     Serial.println("Remove all weight from scale");
     Serial.println("After readings begin, place known weight on scale");
-    Serial.println("Press q,s,d,f,g to increase calibration factor by 1,10,100,1000,10000 respectively");
-    Serial.println("Press w,x,c,v,b to decrease calibration factor by 1,10,100,1000,10000 respectively");
-    Serial.println("Press t for tare");
+    Serial.println("Press + or a to increase calibration factor");
+    Serial.println("Press - or z to decrease calibration factor");
+
     scale.begin(DOUT, CLK);
     scale.set_scale();
-    scale.tare(); //Reset the scale to 0
-
-    long zero_factor = scale.read_average(); //Get a baseline reading
-    Serial.print("Zero factor: ");           //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+    scale.tare();                            // Reset scale to 0
+    long zero_factor = scale.read_average(); // Get basic reading
+    Serial.print("Zero factor: ");           // This can be used to eliminate the need to tare the scale. Useful in constant scale projects.
     Serial.println(zero_factor);
-    calibration_factor = -1500.00;
 }
-
-//=============================================================================================
-//                         LOOP
-//=============================================================================================
 
 void loop()
 {
 
-    scale.set_scale(calibration_factor); //Adjust to this calibration factor
-
+    scale.set_scale(Calibration_Factor_Of_Load_cell); // Adjust this calibration factor
     Serial.print("Reading: ");
-    Serial.print(scale.get_units(), 3);
-    Serial.print(" g"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
-    Serial.print(" calibration_factor: ");
-    Serial.print(calibration_factor);
+    U = scale.get_units();
+    if (U < 0)
+    {
+        U = 0.00;
+    }
+    O = U * 0.035274;
+    Serial.print(O);
+    Serial.print(" grams");
+    Serial.print(" Calibration_Factor_Of_Load_cell: ");
+    Serial.print(Calibration_Factor_Of_Load_cell);
     Serial.println();
 
     if (Serial.available())
     {
         char temp = Serial.read();
         delay(2);
-        Serial.print(temp);
-        if (temp == '+' || temp == 'q')
-            calibration_factor += 1;
-        else if (temp == '-' || temp == 'w')
-            calibration_factor -= 1;
-        else if (temp == 's')
-            calibration_factor += 10;
-        else if (temp == 'x')
-            calibration_factor -= 10;
-        else if (temp == 'd')
-            calibration_factor += 100;
-        else if (temp == 'c')
-            calibration_factor -= 100;
-        else if (temp == 'f')
-            calibration_factor += 1000;
-        else if (temp == 'v')
-            calibration_factor -= 1000;
-        else if (temp == 'g')
-            calibration_factor += 10000;
-        else if (temp == 'b')
-            calibration_factor -= 10000;
-        else if (temp == 't')
-            scale.tare(); //Reset the scale to zero
+        if (temp == '+' || temp == 'a')
+            Calibration_Factor_Of_Load_cell += 0.5;
+        else if (temp == '-' || temp == 'z')
+            Calibration_Factor_Of_Load_cell -= 0.5;
     }
 }
