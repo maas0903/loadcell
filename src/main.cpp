1651e772684705fe0741167d1a5b6a0a0bbe7a4c
Marius <maas0903@gmail.com>
2022-04-24 00:22:07 +0200

    single readings and tare button

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,58 @@
 #include <HX711.h> //You must have this library in your arduino library folder
+#include <Pushbutton.h>
 
 #define CLK 22
 #define DOUT 23
+#define BUTTON_PIN 19
+Pushbutton button(BUTTON_PIN);
 
 HX711 scale;
+int reading;
+int lastReading;
 
 float Calibration_Factor_Of_Load_cell = -8.1;
-float U;
-float O;
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("HX711 calibration sketch");
-    Serial.println("Remove all weight from scale");
-    Serial.println("After readings begin, place known weight on scale");
-    Serial.println("Press + or a to increase calibration factor");
-    Serial.println("Press - or z to decrease calibration factor");
-
     scale.begin(DOUT, CLK);
-    scale.set_scale();
-    scale.tare();                            // Reset scale to 0
-    long zero_factor = scale.read_average(); // Get basic reading
-    Serial.print("Zero factor: ");           // This can be used to eliminate the need to tare the scale. Useful in constant scale projects.
-    Serial.println(zero_factor);
+    scale.set_scale(Calibration_Factor_Of_Load_cell);
+    scale.tare(); // Reset scale to 0
+    Serial.println("starting");
 }
 
 void loop()
 {
+    if (button.getSingleDebouncedPress())
+    {
+        Serial.print("tare...");
+        scale.tare();
+    }
 
-    scale.set_scale(Calibration_Factor_Of_Load_cell); // Adjust this calibration factor
-    Serial.print("Reading: ");
+    delay(200);
+
+    if (scale.wait_ready_timeout(200))
+    {
+        reading = round(scale.get_units());
+        /*if (reading < 0)
+        {
+            reading = 0.00;
+        }*/
+        reading = reading * 0.035274;
+
+        if (reading != lastReading)
+        {
+            Serial.print("Weight: ");
+            Serial.println(reading);
+        }
+        lastReading = reading;
+    }
+    else
+    {
+        Serial.println("HX711 not found.");
+    }
+
+    /*Serial.print("Reading: ");
     U = scale.get_units();
     if (U < 0)
     {
@@ -52,4 +74,5 @@ void loop()
         else if (temp == '-' || temp == 'z')
             Calibration_Factor_Of_Load_cell -= 0.5;
     }
+    */
 }
