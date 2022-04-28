202dde6af489a310fc8cc6ec7bd0b43ae1e0ab99
Marius <maas0903@gmail.com>
2022-04-28 23:06:03 +0200

    added oled display

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <HX711.h>
 #include <Pushbutton.h>
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define CLK 22
+//#define CLK 22
+#define CLK 5
 #define DOUT 23
+
 #define BUTTON_PIN 19
 Pushbutton button(BUTTON_PIN);
 
@@ -14,6 +18,10 @@ int lastReading;
 
 float Calibration_Factor_Of_Load_cell = 8.1;
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup()
 {
     Serial.begin(115200);
@@ -21,6 +29,26 @@ void setup()
     scale.set_scale(Calibration_Factor_Of_Load_cell);
     scale.tare();
     Serial.println("starting");
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;
+    }
+    delay(2000);
+    display.clearDisplay();
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 17);
+    display.println("Starting");
+    display.display();
+    delay(3000);
+    display.clearDisplay();
+    display.setCursor(10, 17);
+    display.println("Ready");
+    display.display();
 }
 
 void loop()
@@ -36,16 +64,19 @@ void loop()
     if (scale.wait_ready_timeout(200))
     {
         reading = round(scale.get_units());
-        /*if (reading < 0)
-        {
-            reading = 0.00;
-        }*/
-        reading = reading * 0.035274;
+
+        reading = reading * 0.035274; // ounce to gram
 
         if ((reading > lastReading + 1) or (reading < lastReading - 1))
         {
             Serial.print("Weight: ");
             Serial.println(reading);
+
+            display.clearDisplay();
+            display.setCursor(0, 17);
+            display.print(String(reading) + "g");
+            display.display();
+            // delay(3000);
         }
         lastReading = reading;
     }
